53c4eea91cbbf6e917ae1ff39c92a0d1cafbb0b3
FancyChaos <Felixs.Developer@tutanota.com>
2021-10-06 10:03:26 +0200

    Move clock to far right side

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/        /*Command*/     /*Update Interval*/     /*Update Signal*/
-        {"",    "sb-clock",             30,             1},
         {"",    "sb-cpu",               5,              2},
         {"",    "sb-memory",            10,             3},
         {"",    "sb-disk",              60,             4},
         {"",    "sb-battery",           30,             5},
         {"",    "sb-internet",          5,              6},
+        {"",    "sb-clock",             30,             1},
 };
 
 
